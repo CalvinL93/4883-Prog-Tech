19d61fc43068ff40ae19859f8de6529dac6e3e62
CalvinL93 <CalvinL93@yahoo.com>
2023-09-21 14:36:57 -0500

    Modified main a07

--- Assignments/A07/main.cpp
+++ Assignments/A07/main.cpp
@@ -1,33 +1,61 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 int main() {
-    int size, x, y;
-    bool same = true;
+    int size, x, y, z;
+    stack<int> cars;
+    //bool same = true;
 
     while(cin >> x) {
       size = y = x;
       
       while(x != 0) {
-        while(size != 0) {
-          cin >> x;
-          if (x == 0) 
-            break;
-          if (x != size) {
-            same = false;
-          }
-          size--;
+        cin >> x;
+        if (z < size) {
+          cars.push(z);
+          z++;
+          break;
         }
-        if (x == 0) 
+        if(x == z) {
+          cars.pop();
+          y--;
+        }
+        if(y != 0) {
+          if(z != size) {
+            cars.push(z);
+            z++;
+          }
+          else {
+            cout << "No\n";
             break;
-        if (size == 0 && same != false) 
+          }
+        }
+        if (y == 0 && z == size) {
           cout << "Yes\n";
-        if (same == false)
-          cout << "No\n";
-        size = y;
-        same = true;
+        }
       }
+
+      // while(x != 0) {
+      //   while(size != 0) {
+      //     cin >> x;
+      //     if (x == 0) 
+      //       break;
+      //     if (x != size) {
+      //       same = false;
+      //     }
+      //     size--;
+      //   }
+      //   if (x == 0) 
+      //       break;
+      //   if (size == 0 && same != false) 
+      //     cout << "Yes\n";
+      //   if (same == false)
+      //     cout << "No\n";
+      //   size = y;
+      //   same = true;
+      // }
         cout << "\n";
     } 
 }
