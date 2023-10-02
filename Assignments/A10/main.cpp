6a5497ded1c5cd4deab64786505f3c3a524db481
CalvinL93 <calvinl93@yahoo.com>
2023-10-02 17:17:07 -0500

    Add a10

---
+++ Assignments/A10/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t, n, r, u, v;
+
+    cin >> t;
+
+    while(t--) {
+        cin >> n;
+        cin >> r;
+
+        
+            
+    }
+}
